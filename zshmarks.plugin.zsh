665f64c1d322c511dd06a6fd5005d541d1538457
Revanth Revoori <revanth001reva@gmail.com>
2017-03-07 21:36:16 -0600

    Added fuzzy to jump and multiple deletes to deletemark

--- zshmarks.plugin.zsh
+++ zshmarks.plugin.zsh
@@ -13,7 +13,7 @@ fi
 
 # Check if $BOOKMARKS_FILE is a symlink.
 if [[ -L $BOOKMARKS_FILE ]]; then
-  BOOKMARKS_FILE=$(readlink $BOOKMARKS_FILE)
+	BOOKMARKS_FILE=$(readlink $BOOKMARKS_FILE)
 fi
 
 # Create bookmarks_file it if it doesn't exist
@@ -21,41 +21,45 @@ if [[ ! -f $BOOKMARKS_FILE ]]; then
 	touch $BOOKMARKS_FILE
 fi
 
+fzfcmd() {
+   [ ${FZF_TMUX:-1} -eq 1 ] && echo "fzf-tmux -d${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
+}
+
 _zshmarks_move_to_trash(){
-  if [[ $(uname) == "Linux"* || $(uname) == "FreeBSD"*  ]]; then
-    label=`date +%s`
-    mkdir -p ~/.local/share/Trash/info ~/.local/share/Trash/files
-    \mv "${BOOKMARKS_FILE}.bak" ~/.local/share/Trash/files/bookmarks-$label
-    echo "[Trash Info]
+	if [[ $(uname) == "Linux"* || $(uname) == "FreeBSD"*  ]]; then
+		label=`date +%s`
+		mkdir -p ~/.local/share/Trash/info ~/.local/share/Trash/files
+		\mv "${BOOKMARKS_FILE}.bak" ~/.local/share/Trash/files/bookmarks-$label
+		echo "[Trash Info]
 Path=/home/"$USER"/.bookmarks
 DeletionDate="`date +"%Y-%m-%dT%H:%M:%S"`"
 ">~/.local/share/Trash/info/bookmarks-$label.trashinfo
-  elif [[ $(uname) = "Darwin" ]]; then
-    \mv "${BOOKMARKS_FILE}.bak" ~/.Trash/"bookmarks"$(date +%H-%M-%S)
-  else
-    \rm -f "${BOOKMARKS_FILE}.bak"
-  fi
+	elif [[ $(uname) = "Darwin" ]]; then
+		\mv "${BOOKMARKS_FILE}.bak" ~/.Trash/"bookmarks"$(date +%H-%M-%S)
+	else
+		\rm -f "${BOOKMARKS_FILE}.bak"
+	fi
 }
 
 function bookmark() {
 	local bookmark_name=$1
 	if [[ -z $bookmark_name ]]; then
-        bookmark_name="${PWD##*/}"
-    fi
-    cur_dir="$(pwd)"
-    # Replace /home/uname with $HOME
-    if [[ "$cur_dir" =~ ^"$HOME"(/|$) ]]; then
-        cur_dir="\$HOME${cur_dir#$HOME}"
-    fi
-    # Store the bookmark as folder|name
-    bookmark="$cur_dir|$bookmark_name"
-    if [[ -z $(grep "$bookmark" $BOOKMARKS_FILE 2>/dev/null) ]]; then
-        echo $bookmark >> $BOOKMARKS_FILE
-        echo "Bookmark '$bookmark_name' saved"
-    else
-        echo "Bookmark already existed"
-        return 1
-    fi
+				bookmark_name="${PWD##*/}"
+		fi
+		cur_dir="$(pwd)"
+		# Replace /home/uname with $HOME
+		if [[ "$cur_dir" =~ ^"$HOME"(/|$) ]]; then
+				cur_dir="\$HOME${cur_dir#$HOME}"
+		fi
+		# Store the bookmark as folder|name
+		bookmark="$cur_dir|$bookmark_name"
+		if [[ -z $(grep "$bookmark" $BOOKMARKS_FILE 2>/dev/null) ]]; then
+				echo $bookmark >> $BOOKMARKS_FILE
+				echo "Bookmark '$bookmark_name' saved"
+		else
+				echo "Bookmark already existed"
+				return 1
+		fi
 }
 
 __zshmarks_zgrep() {
@@ -75,6 +79,10 @@ __zshmarks_zgrep() {
 
 function jump() {
 	local bookmark_name=$1
+	if [ $# -eq 0 ]; then
+    	local jumpline=$(cat ${BOOKMARKS_FILE} | $(fzfcmd) --bind=ctrl-y:accept --tac)
+    	eval cd "${jumpline%%|*}"
+	else
 	local bookmark
 	if ! __zshmarks_zgrep bookmark "\\|$bookmark_name\$" "$BOOKMARKS_FILE"; then
 		echo "Invalid name, please provide a valid bookmark name. For example:"
@@ -87,50 +95,60 @@ function jump() {
 		local dir="${bookmark%%|*}"
 		eval "cd \"${dir}\""
 	fi
+fi
 }
 
 # Show a list of the bookmarks
 function showmarks() {
-  local bookmark_file="$(<"$BOOKMARKS_FILE")"
-  local bookmark_array; bookmark_array=(${(f)bookmark_file});
-  local bookmark_name bookmark_path bookmark_line
-  if [[ $# -eq 1 ]]; then
-    bookmark_name="*\|${1}"
-    bookmark_line=${bookmark_array[(r)$bookmark_name]}
-    bookmark_path="${bookmark_line%%|*}"
-    bookmark_path="${bookmark_path/\$HOME/~}"
-    printf "%s \n" $bookmark_path
-  else
-    for bookmark_line in $bookmark_array; do
-      bookmark_path="${bookmark_line%%|*}"
-      bookmark_path="${bookmark_path/\$HOME/~}"
-      bookmark_name="${bookmark_line#*|}"
-      printf "%s\t\t%s\n" "$bookmark_name" "$bookmark_path"
-    done
-  fi
+	local bookmark_file="$(<"$BOOKMARKS_FILE")"
+	local bookmark_array; bookmark_array=(${(f)bookmark_file});
+	local bookmark_name bookmark_path bookmark_line
+	if [[ $# -eq 1 ]]; then
+		bookmark_name="*\|${1}"
+		bookmark_line=${bookmark_array[(r)$bookmark_name]}
+		bookmark_path="${bookmark_line%%|*}"
+		bookmark_path="${bookmark_path/\$HOME/~}"
+		printf "%s \n" $bookmark_path
+	else
+		for bookmark_line in $bookmark_array; do
+			bookmark_path="${bookmark_line%%|*}"
+			bookmark_path="${bookmark_path/\$HOME/~}"
+			bookmark_name="${bookmark_line#*|}"
+			printf "%s\t\t%s\n" "$bookmark_name" "$bookmark_path"
+		done
+	fi
 }
 
 # Delete a bookmark
 function deletemark()  {
-  local bookmark_name=$1
-  if [[ -z $bookmark_name ]]; then
-    printf "%s \n" "Please provide a name for your bookmark to delete. For example:"
-    printf "\t%s \n" "deletemark foo"
-    return 1
-  else
-    local bookmark_line bookmark_search
-    local bookmark_file="$(<"$BOOKMARKS_FILE")"
-    local bookmark_array; bookmark_array=(${(f)bookmark_file});
-    bookmark_search="*\|${bookmark_name}"
-    if [[ -z ${bookmark_array[(r)$bookmark_search]} ]]; then
-      eval "printf '%s\n' \"'${bookmark_name}' not found, skipping.\""
-    else
-      \cp "${BOOKMARKS_FILE}" "${BOOKMARKS_FILE}.bak"
-      bookmark_line=${bookmark_array[(r)$bookmark_search]}
-      bookmark_array=(${bookmark_array[@]/$bookmark_line})
-      eval "printf '%s\n' \"\${bookmark_array[@]}\"" >! $BOOKMARKS_FILE
-      _zshmarks_move_to_trash
-    fi
+	local bookmark_name=$1
+	if [[ -z $bookmark_name ]]; then
+	    local marks_to_delete line
+	    marks_to_delete=$(cat $BOOKMARKS_FILE | $(fzfcmd) -m --bind=ctrl-y:accept,ctrl-t:toggle-up --tac)
+
+	    if [[ -n ${marks_to_delete} ]]; then
+	        while read -r line; do
+	            perl -p -i -e "s#${line}\n##g" $BOOKMARKS_FILE
+	        done <<< "$marks_to_delete"
+
+	        echo "** The following marks were deleted **"
+	        echo "${marks_to_delete}"
+	    fi
+	else
+		local bookmark_line bookmark_search
+		local bookmark_file="$(<"$BOOKMARKS_FILE")"
+		local bookmark_array; bookmark_array=(${(f)bookmark_file});
+		bookmark_search="*\|${bookmark_name}"
+		if [[ -z ${bookmark_array[(r)$bookmark_search]} ]]; then
+			eval "printf '%s\n' \"'${bookmark_name}' not found, skipping.\""
+		else
+			\cp "${BOOKMARKS_FILE}" "${BOOKMARKS_FILE}.bak"
+			bookmark_line=${bookmark_array[(r)$bookmark_search]}
+			bookmark_array=(${bookmark_array[@]/$bookmark_line})
+			eval "printf '%s\n' \"\${bookmark_array[@]}\"" >! $BOOKMARKS_FILE
+			_zshmarks_move_to_trash
+		fi
 	fi
+fi
 }
 
